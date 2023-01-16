19f86fe86f5127d71177e73db2dc3f83e6e81d7d
oltiademi <olti.ademi@student.uni-pr.edu>
2023-01-16 01:20:26 +0100

    modified the home page

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,20 @@
-const button = document.getElementById("butoniPerMesazhOlti");
-button.addEventListener("click", function(){
-    const recipient = "oltiademi30@gmail.com";
-    const gmailComposeUrl = https://mail.google.com/mail/?view=cm&fs=1&to=${recipient};
-    window.location.href = gmailComposeUrl;
-});
+document.getElementById("butoniPerMesazhOlti").addEventListener("click", function(){
+    window.location.href = "https://mail.google.com/mail/?view=cm&fs=1&to=oltiademi30@gmail.com";
+ });
+ document.getElementById("butoniPerMesazhLeo").addEventListener("click", function(){
+    window.location.href = "https://mail.google.com/mail/?view=cm&fs=1&to=leutrimhajdini63@gmail.com";
+ });
+const btn = document.getElementById("butoni-thirrjeOlti");
+btn.addEventListener("click", function(){
+    
+     if(btn.textContent==="End Call"){
+       btn.textContent="Call";
+    }
+    else{
+        btn.textContent="End Call";
+       
+    }
+ });
+ 
+
+ 
