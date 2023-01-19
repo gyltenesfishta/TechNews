b34ee558124e25d44f70c1686d0bb6a183232ff5
oltiademi <olti.ademi@student.uni-pr.edu>
2023-01-19 19:48:52 +0100

    Finalizing and implementing the footer in each page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#submit").click(function() {
+      alert("You successfully signed up!");
+    });
+  });
+  $(navbarDropdownMenuLink).ready(function() {
+     $('.dropdown-toggle').dropdown()
+   });
